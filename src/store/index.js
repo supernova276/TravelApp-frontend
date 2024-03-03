409107658f9198eeaa518d8ee33a1fd7c59d6f5e
Akshita <akshita.dixit2706@gmail.com>
2024-03-03 23:16:45 +0530

    added the new redux-devtools-extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import {legacy_createStore,applyMiddleware} from 'redux'
 import {thunk} from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import {rootReducer} from '../reducers/index';
 
 const store=legacy_createStore(
